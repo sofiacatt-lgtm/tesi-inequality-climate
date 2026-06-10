3b7a9b9b61007d469029f7fc386a46ab10ccbd65
sofiacatt-lgtm <sofia.catt@gmail.com>
2026-06-10 13:00:08 +0200

    Aggiunto modifiche shock

--- src/conditions.py
+++ src/conditions.py
@@ -3,89 +3,90 @@
 from parameters import *
 from functions import *
 
-def Primacondizione(mu, r, alpha, xF, EA_up, Ybar):
-    """Diseguaglianza 35: in xF deve stare sotto la bisettrice da sx"""
-    if (EA_up()*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF <0):
+def Primacondizione(mu, r, alpha, xF, A_up_bar, Ybar):
+    """Diseguaglianza 35: in xF deve stare sotto la bisettrice da sx (expected)"""
+    if (EA_up(A_up_bar)*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF <0):
         return True
     else :
         return False
 
-def Secondacondizione(mu, r, alpha, xF, EA_p_min, Ybar):
-    """Diseguaglianza 36: in xF deve stare sopra la bisettrice da dx"""
-    if (EA_p_min()*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF >0):
+def Secondacondizione(mu, r, alpha, xF, A_p_bar, Ybar):
+    """Diseguaglianza 36: in xF deve stare sopra la bisettrice da dx(expected)"""
+    if (EA_p_min(A_p_bar)*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF >0):
         return True
     else :
         return False
 
-def Terzacondizione(mu, r, alpha, xF, EA_p_min, Ybar):
-    """Diseguaglianza 37: slope dx < 1 almeno nel valore minimo"""
-    if (EA_p_min()*(1+mu) - mu*(1+r) - 1/(1-alpha) <0):
+def Terzacondizione(mu, r, alpha, xF, A_p_bar, Ybar):
+    """Diseguaglianza 37: slope dx < 1 almeno nel valore minimo(expected)"""
+    if (EA_p_min(A_p_bar)*(1+mu) - mu*(1+r) - 1/(1-alpha) <0):
         return True
     else :
         return False
 
-def Quartacondizione(mu, r, alpha, xF, EA_p_max, Ybar):
-    """Diseguaglianza 47: slope dx >1 almeno nel valore massimo"""
-    if (EA_p_max()*(1+mu) - mu*(1+r) - 1/(1-alpha) >0):
+def Quartacondizione(mu, r, alpha, xF, A_p_bar, Ybar):
+    """Diseguaglianza 47: slope dx >1 almeno nel valore massimo(expected)"""
+    if (EA_p_max(A_p_bar)*(1+mu) - mu*(1+r) - 1/(1-alpha) >0):
         return True
     else :
         return False
 
 
-#funzioni utili con gli shock
+#no shock
 
-def Primacondizionenoshock(mu, r, alpha, xF, A_up_noshock, Ybar):
+def Primacondizionenoshock(mu, r, alpha, xF, A_up_bar, Ybar):
     """Diseguaglianza 35 senza shock: in xF deve stare sotto la bisettrice da sx"""
-    if (A_up_noshock()*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF <0):
+    if (A_up_noshock(A_up_bar)*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF <0):
         return True
     else :
         return False
 
-def Secondacondizionenoshock(mu, r, alpha, xF, A_p_min, Ybar):
+def Secondacondizionenoshock(mu, r, alpha, xF, A_p_bar, Ybar):
     """Diseguaglianza 36 senza shock: in xF deve stare sopra la bisettrice da dx nel valore minimo"""
-    if (A_p_noshock(0)*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF >0):
+    if (A_p_noshock(A_p_bar, 0)*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF >0):
         return True
     else :
         return False
 
-def Terzacondizionenoshock(mu, r, alpha, xF, A_p_min, Ybar):
+def Terzacondizionenoshock(mu, r, alpha, xF, A_p_bar, Ybar):
     """Diseguaglianza 37 senza shock: slope dx < 1 almeno nel valore minimo"""
-    if (A_p_min()*(1+mu) - mu*(1+r) - 1/(1-alpha) <0):
+    if (A_p_noshock(A_p_bar, 0)*(1+mu) - mu*(1+r) - 1/(1-alpha) <0):
         return True
     else :
         return False
 
-def Quartacondizionenoshock(mu, r, alpha, xF, A_p_max, Ybar):
+def Quartacondizionenoshock(mu, r, alpha, xF, A_p_bar, Ybar):
     """Diseguaglianza 47 senza shock: slope dx >1 almeno nel valore massimo"""
-    if (A_p_max()*(1+mu) - mu*(1+r) - 1/(1-alpha) >0):
+    if (A_p_noshock(A_p_bar, 1)*(1+mu) - mu*(1+r) - 1/(1-alpha) >0):
         return True
     else :
         return False
 
-def Primacondizioneshock(mu, r, alpha, xF, A_up_shock, Ybar):
+#shock
+def Primacondizioneshock(mu, r, alpha, xF, A_up_bar, Ybar):
     """Diseguaglianza 35: in xF deve stare sotto la bisettrice da sx"""
-    if (A_up_shock()*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF <0):
+    if (A_up_shock(A_up_bar)*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF <0):
         return True
     else :
         return False
 
-def Secondacondizioneshock(mu, r, alpha, xF, A_p_shock, Ybar ):
+def Secondacondizioneshock(mu, r, alpha, xF, A_p_bar, Ybar ):
     """Diseguaglianza 36: in xF deve stare sopra la bisettrice da dx nel valore minimo"""
-    if (A_p_shock(0)*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF >0):
+    if (A_p_shock(A_p_bar, 0)*(1+mu) - mu*(1+r) - 1/(1-alpha) + Ybar/xF >0):
         return True
     else :
         return False
 
-def Terzacondizioneshock(mu, r, alpha, xF, A_p_shock, Ybar):
+def Terzacondizioneshock(mu, r, alpha, xF, A_p_bar, Ybar):
     """Diseguaglianza 37: slope dx < 1 almeno nel valore minimo"""
-    if (A_p_shock(0)*(1+mu) - mu*(1+r) - 1/(1-alpha) <0):
+    if (A_p_shock(A_p_bar, 0)*(1+mu) - mu*(1+r) - 1/(1-alpha) <0):
         return True
     else :
         return False
 
-def Quartacondizioneshock(mu, r, alpha, xF, A_p_shock, Ybar):
+def Quartacondizioneshock(mu, r, alpha, xF, A_p_bar, Ybar):
     """Diseguaglianza 47: slope dx >1 almeno nel valore massimo"""
-    if (A_p_shock(1)*(1+mu) - mu*(1+r) - 1/(1-alpha) >0):
+    if (A_p_shock(A_p_bar, 1)*(1+mu) - mu*(1+r) - 1/(1-alpha) >0):
         return True
     else :
         return False
--- src/functions.py
+++ src/functions.py
@@ -14,6 +14,7 @@ def EA_p(A_p_bar, n_p):
 def EA_up(A_up_bar):
     return A_up_bar * (1 - eta_up)
 
+
 def A_p_shock(A_p_bar, n_p):
     return A_p_bar * (1 - gamma_p) * (1 + n_p)
 
@@ -25,6 +26,7 @@ def A_p_noshock(A_p_bar, n_p):
 
 def A_up_noshock(A_up_bar):
     return A_up_bar
+
 
 def xp(mu, r, alpha, A_up_bar, Ybar):
     return (1-alpha)*Ybar / (1 - (1-alpha)*((1+mu)*EA_up(A_up_bar) - mu*(1+r)))
